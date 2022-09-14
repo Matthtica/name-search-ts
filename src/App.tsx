b108c6e2c6ec823a95fac08cee6bb2119eab238d
Matthtica <zxnightstalker199733@gmail.com>
2022-09-14 20:58:11 +0630

    testing xlsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useCallback, useEffect, useState } from 'react';
+import { read, utils, writeFile } from 'xlsx';
 import './App.css';
 
+interface President {
+    Name: string;
+    Index: number;
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [pres, setPres] = useState<President[]>();
+
+    useEffect(() => {
+        (async () => {
+            const f = await (await fetch("https://sheetjs.com/pres.xlsx")).arrayBuffer();
+            const wb = read(f);
+            const ws = wb.Sheets[wb.SheetNames[0]];
+            const data = utils.sheet_to_json(ws);
+            setPres(data as President[]);
+        })();
+    }, []);
+
+    return (
+        <div className="App">
+            <table>
+                <thead>
+                    <th>Name</th>
+                    <th>Index</th>
+                </thead>
+                <tbody>
+                    {
+                        pres?.map(p => (<tr>
+                            <td>{p.Name}</td>
+                            <td>{p.Index}</td>
+                        </tr>))
+                    }
+                </tbody>
+                <tfoot>
+                </tfoot>
+            </table>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Item.tsx
@@ -0,0 +1,9 @@
+import styles from './Item.module.css'
+
+export default function Item() {
+    return (
+        <div>
+            <h1>What?</h1>
+        </div>
+    )
+}
