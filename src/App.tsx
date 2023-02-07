a96d959a9e8df2d26c379f3aceae7117311bbc7d
Matthtica <zxnightstalker199733@gmail.com>
2023-02-07 15:04:35 +0630

    Testing authentication in production.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,35 @@
 import style from './App.module.scss';
 import Table from './components/Table';
-import firebaseui from 'firebaseui';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { GoogleAuthProvider, signInWithPopup, UserCredential } from 'firebase/auth'
+import { auth } from './firebase';
 
 function App() {
 
-    const ui = new firebaseui.auth.AuthUI(fire)
+    const [user] = useAuthState(auth)
 
     return (
         <div className={style.cont}>
             <div className={style.box}>
                 <Table />
+                <SignIn/>
             </div>
         </div>
     );
+}
+
+function SignIn() {
+    const signInWithGoogle = async () => {
+        const provider = new GoogleAuthProvider();
+        signInWithPopup(auth, provider)
+            .then((result: UserCredential) => {
+                console.log(result.user);
+            }).catch((error) => {
+                console.log("What the fuck just happened?")
+                console.log(error);
+            });
+    }
+    return <button className={style.signin} onClick={signInWithGoogle}>Do not press this button!!!</button>
 }
 
 export default App;
